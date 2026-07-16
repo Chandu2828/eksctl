9a83d44d3a24970f2935c7066897b20d24752ed5
Chandu2828 <purnachand7569@gmail.com>
2026-07-16 18:43:49 +0530

    added files

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "aws_access_key" {
+    description = "AWS Access key ID for configuring the workstation"
+    type        = string 
+    sensitive   = true 
+}
+
+variable "aws_secret_key" {
+    description = "AWS Secret Access Key for configuring the workstation"
+    type        = string 
+    sensitive   = true 
+}
+
+variable "ssh_password" {
+    description = "SSH password for ec2-user to run destory-time provisioner"
+    type        = string 
+    sensitive   = true 
+}
+
